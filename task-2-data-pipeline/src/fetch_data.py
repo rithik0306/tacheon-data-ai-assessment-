04eb8fcfe979f4e078ff197724886da76073dc2a
Rithik0316 <120165108+rithik0306@users.noreply.github.com>
2026-05-29 13:30:32 +0530

    Added API data fetching script

---
+++ task-2-data-pipeline/src/fetch_data.py
@@ -0,0 +1,35 @@
+import requests
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+API_URL = "https://api.coingecko.com/api/v3/coins/markets"
+
+PARAMS = {
+    "vs_currency": "usd",
+    "order": "market_cap_desc",
+    "per_page": 10,
+    "page": 1
+}
+
+def fetch_crypto_data():
+    try:
+        logging.info("Fetching data from CoinGecko API...")
+
+        response = requests.get(API_URL, params=PARAMS)
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        logging.info("Data fetched successfully.")
+
+        return data
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"API request failed: {e}")
+        return None
+
+
+if __name__ == "__main__":
+    fetch_crypto_data()
