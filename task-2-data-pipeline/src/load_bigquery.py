f079096c68e8d4f25393c14223ef7ef3091aded1
Rithik0316 <120165108+rithik0306@users.noreply.github.com>
2026-05-29 13:32:12 +0530

    Added BigQuery loading functionality

---
+++ task-2-data-pipeline/src/load_bigquery.py
@@ -0,0 +1,16 @@
+from google.cloud import bigquery
+
+def load_to_bigquery(df):
+
+    client = bigquery.Client()
+
+    table_id = "YOUR_PROJECT_ID.crypto_data.crypto_market_data"
+
+    job = client.load_table_from_dataframe(
+        df,
+        table_id
+    )
+
+    job.result()
+
+    print("Data loaded successfully into BigQuery.")
